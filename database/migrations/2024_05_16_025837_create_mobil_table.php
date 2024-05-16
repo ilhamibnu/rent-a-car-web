9d60e6dd94c46925918b912781280b0a58f94991
ilhamibnu <e41210178@student.polije.ac.id>
2024-05-16 10:33:02 +0700

    feat migration

--- database/migrations/2024_05_16_025837_create_mobil_table.php
+++ database/migrations/2024_05_16_025837_create_mobil_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mobil', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('user');
-            $table->rememberToken();
+            $table->string('nama');
+            $table->string('foto');
+            $table->longText('deskripsi');
+            $table->string('harga');
+            $table->string('status')->default('tersedia');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mobil');
     }
 };
---
+++ database/migrations/2024_05_16_030253_add_id_mobil_column_to_detail_transaksi_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('detail_transaksi', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_mobil')->after('id');
+            $table->foreign('id_mobil')->references('id')->on('mobil');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('detail_transaksi', function (Blueprint $table) {
+            $table->dropForeign(['id_mobil']);
+            $table->dropColumn('id_mobil');
+        });
+    }
+};
